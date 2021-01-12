e883633f2c548d40a6e34ee98d325cfc50418af0
5axes <5axes@users.noreply.github.com>
2021-01-12 14:51:34 +0100

    Update ReplaceM73.py

--- ReplaceM73.py
+++ ReplaceM73.py
@@ -5,6 +5,9 @@ import os
 import os.path
 from os import path
 
+# Need to add : ;LAYER_COUNT:{total_layer_count}
+# in the Prusa Slicer Initial GCode 
+
 # first argument passed by PrusaSlicer = Gcode file
 sourceFile=sys.argv[1]
 
@@ -22,17 +25,29 @@ os.rename(sourceFile,destFile+".bak")
 destFile = re.sub('\.gcode$','',sourceFile)
 destFile = destFile + '.gcode'
 
+# Init Layer_Numb = 0
+layer_num = 0
+total_layer_count = 0
 # Rewrite the ENTIRE Gcode file
 with open(destFile, "w") as of:
     for lIndex in range(len(lines)):
         oline = lines[lIndex]
+        
+        if oline[:13] == ";LAYER_COUNT:":
+            total_layer_count = int(oline.split(":")[1])
+        if oline[:13] == ";LAYER_CHANGE":
+            layer_num += 1
         if oline[:3] == "M73":
             # format M117 %12 3h37m
             percent = oline.replace("M73 P","").split(" ")[0]
             total_time = int(oline.split("R")[1])
             h, m = divmod(total_time, 60)    # hours, minutes
             total_time_string = " {:d}h{:d}m\n".format( int(h), int(m))
-            M117_code = "M117 %" + percent
+            if total_layer_count > 0 :
+                M117_code = "M117 ({:d}/{:d}) %".format( int(layer_num), int(total_layer_count))
+            else:
+                M117_code = "M117 %"
+            M117_code = M117_code + percent
             tempLine = M117_code + total_time_string
             of.write(tempLine)
         else:
